9900d9c04c204c837ee5ff0c0472568932afa31e
ChiamYu Ng <ngchiamyu@gmail.com>
2018-10-11 00:25:23 -0700

    Revert drawing config

--- optstoicpy/core/config.py
+++ optstoicpy/core/config.py
@@ -66,3 +66,43 @@ default_params = {
     'RATIO_BOUND': ratio_bound,
     'RATIO': ratio
 }
+
+
+### Color configurations for drawpathway.py
+color_configs = {}
+color_configs['light'] = dict(COFACTOR_SHAPE="ellipse",  # "diamond"
+                              OTHER_COFACTOR_COLOR='#B6B6B6',
+                              NONCOFACTOR_SHAPE="plaintext",  # "box"
+                              NONCOFACTOR_COLOR='transparent',  # "#D2EBEB"
+                              REACTION_COLOR="#512DA8",
+                              RXN_NODE_COLOR="#323232",
+                              EDGE_COLOR="#323232",  # "#505050"
+                              BACKGROUND_COLOR="transparent",
+                              ALL_FONT_COLOR="black")
+# color_configs['light']['colorMapping'] = {
+#     'C00002': '#F05456', 'C00008': '#F05456',
+#     'C00003': '#FFEB3B', 'C00004': '#FFEB3B',
+#     'C00005': '#9dd3ee', 'C00006': '#9dd3ee'
+# }
+
+color_configs['light']['colorMapping'] = {
+    'C00002': '#F05456', 'C00008': '#FFC000',
+    'C00003': '#149B76', 'C00004': '#149B76',
+    'C00005': '#2393CB', 'C00006': '#2393CB'
+}
+
+# For blue background
+color_configs['dark'] = dict(COFACTOR_SHAPE="ellipse",  # "diamond"
+                             OTHER_COFACTOR_COLOR="#7F7F7F",
+                             NONCOFACTOR_SHAPE="plaintext",  # "box"
+                             NONCOFACTOR_COLOR="transparent",  # "#CCFF33"
+                             REACTION_COLOR="#FFFF00",
+                             EDGE_COLOR="#E5E5E5",  # "#505050"
+                             RXN_NODE_COLOR="#E5E5E5",
+                             BACKGROUND_COLOR="transparent",
+                             ALL_FONT_COLOR="white")
+color_configs['dark']['colorMapping'] = {
+    'C00002': '#F05456', 'C00008': '#FFC000',
+    'C00003': '#149B76', 'C00004': '#149B76',
+    'C00005': '#2393CB', 'C00006': '#2393CB'
+}
